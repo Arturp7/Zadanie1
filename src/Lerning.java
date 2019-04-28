e1033c7fd88bf8e20498af8ac88ff9890f4132b7
Artur Przyjemski <spinezpl@gmail.com>
2019-04-28 18:05:15 +0200

    Dodałem drugi String

--- src/Lerning.java
+++ src/Lerning.java
@@ -1,5 +1,6 @@
 public class Lerning {
     public static void main(String[] args) {
         System.out.println("Uczę się programowania i commitowania");
+        System.out.println("Dodawanie stringów 2 część");
     }
 }
