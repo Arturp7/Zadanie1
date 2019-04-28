10268f9819bc8771c387cb4eaf40c8db4af926c1
Artur Przyjemski <spinezpl@gmail.com>
2019-04-28 18:09:00 +0200

    Doadałem liczbę integer

--- src/Lerning.java
+++ src/Lerning.java
@@ -2,5 +2,7 @@ public class Lerning {
     public static void main(String[] args) {
         System.out.println("Uczę się programowania i commitowania");
         System.out.println("Dodawanie stringów 2 część");
+        System.out.println("Wyświetlenie liczby integer  " + 55555);
+
     }
 }
