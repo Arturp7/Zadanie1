43f5c63261fa973eb7144cf82ef76284ec31923a
Artur Przyjemski <spinezpl@gmail.com>
2019-04-28 18:14:10 +0200

    Jednak znak C jest najlepszy

--- src/Lerning.java
+++ src/Lerning.java
@@ -3,7 +3,7 @@ public class Lerning {
         System.out.println("Uczę się programowania i commitowania");
         System.out.println("Dodawanie stringów 2 część");
         System.out.println("Wyświetlenie liczby integer  " + 55555);
-        System.out.println("Wyświetlenie znaku  " + 'B');
+        System.out.println("Wyświetlenie znaku  " + 'C');
 
 
     }
